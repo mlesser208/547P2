c5fcb974273d440acc5d4d877212706d8db66252
mlesser208 <mlesser@usc.edu>
2025-11-04 11:01:48 -0800

    Project Setup

---
+++ project2_analysis.R
@@ -0,0 +1,27 @@
+# Project 2 Analysis For SSCI 574 by Matthew Lesser
+# Date: Created on OCT 10, 2025
+
+library(sp)
+library(sf)
+library(spdep)
+library(tmap)
+library(ggplot2)
+
+# Set working directory
+# setwd("path/to/your/directory")
+
+# Data import
+# data <- read.csv("data/your_data_file.csv")
+
+# Data analysis
+# Add your analysis code here
+
+# Output
+# Save results to the output folder
+# write.csv(results, "output/results.csv")
+
+# Visualization
+# Create plots and visualizations
+# Add your visualization code here
+
+# End of script
